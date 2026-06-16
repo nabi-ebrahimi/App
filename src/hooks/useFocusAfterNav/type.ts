707fead16c2602fd58cb905ddc52bca9e0ddc32e
Nabi <mnabijan049@gmail.com>
2026-06-16 23:52:11 +0430

    test

--- src/hooks/useFocusAfterNav/type.ts
+++ src/hooks/useFocusAfterNav/type.ts
@@ -3,4 +3,6 @@ import type {AnimatedTextInputRef} from '@components/RNTextInput';
 
 type UseFocusAfterNav = (ref: RefObject<AnimatedTextInputRef | null>, shouldDelayFocus: boolean) => boolean;
 
+//test
+
 export default UseFocusAfterNav;
